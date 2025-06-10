bcdf55ab882749f5375d955f95694dd1f31975ec
Muadz Yamani <muadzyamani@gmail.com>
2025-06-10 16:18:16 +0800

    Add configuration settings for Ollama API and motor control

---
+++ config.py
@@ -0,0 +1,9 @@
+# --- Configuration ---
+OLLAMA_API_URL = "http://localhost:11434/api/generate"
+OLLAMA_MODEL = "phi3:mini"
+SERIAL_PORT = 'COM3'
+SERIAL_BAUDRATE = 115200 # Make sure this matches Arduino
+MOTOR_MIN_ANGLE = 0
+MOTOR_MAX_ANGLE = 180
+MOTOR_DEFAULT_STEP = 15
+MOTOR_INITIAL_ANGLE = 90
